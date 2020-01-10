e75ecd52cd5c257ec8c7f59b2ffa8dd139d42e3b
popigg <popigg@gmail.com>
2020-01-10 21:26:04 +0100

    initial commit

---
+++ LinnearRegression/LinnearRegression.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class LinnearRegression:
+  """
+    Simple Linnear regression
+  """
+  def __init__(self):
+    self._slope = 0
+    self._intercept = 0
+
+  def fit(self, X, y):
+    """
+
+    """
+    self._slope = (np.mean(X.dot(y))- (np.mean(X) * np.mean(y))) \
+    / (np.mean(X.dot(X)) - np.mean(X)**2)
+
+    self._intercept = ((np.mean(y) * np.mean(X.dot(X))) - np.mean(X) * np.mean(X.dot(y))) \
+    / (np.mean(X.dot(X)) - (np.mean(X)**2))
+
+    return self
+
+  def predict(self, x):
+    """
+    """
+    return x * self._slope + self._intercept
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ml_uem',
+      version='0.1',
+      description='my ml package skeleton',
+      packages=['LinnearRegression'],
+      zip_safe=False )
