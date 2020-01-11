ca398f3c8a8be25bc5dba80578b95d39d73c44cd
Zion <ricardosanchez@MacBook-Pro-de-Ricardo.local>
2020-01-11 12:07:19 +0100

    nueva rama richi

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 class LinearRegression:
-  """
+    """
     Simple Linear regression
-  """
-  def __init__(self):
-    self._slope = 0
-    self._intercept = 0
-
-  def fit(self, X, y):
     """
+    def __init__(self):
+        self._slope = 0
+        self._intercept = 0
+        self._predict = 0
 
-    """
-    self._slope = (np.mean(X.dot(y))- (np.mean(X) * np.mean(y))) \
-    / (np.mean(X.dot(X)) - np.mean(X)**2)
+    def fit(self, X, y):
+        self._slope = (np.mean(X.dot(y))- (np.mean(X) * np.mean(y))) \
+        / (np.mean(X.dot(X)) - np.mean(X)**2)
 
-    self._intercept = ((np.mean(y) * np.mean(X.dot(X))) - np.mean(X) * np.mean(X.dot(y))) \
-    / (np.mean(X.dot(X)) - (np.mean(X)**2))
+        self._intercept = ((np.mean(y) * np.mean(X.dot(X))) - np.mean(X) * np.mean(X.dot(y))) \
+        / (np.mean(X.dot(X)) - (np.mean(X)**2))
 
-    return self
+        return self
 
-  def predict(self, x):
-    """
-    """
-    return x * self._slope + self._intercept
+    def predict(self,X):
+        self._predict = X * self._slope + self._intercept
+        return self
+
+    def r_square(self,y):
+        return 1 - ((np.sum((y - self._predict) ** 2)) / (np.sum((y - np.mean(y)) ** 2)))
--- usecases/simple_linear_regression/lr_usecases.py
+++ usecases/simple_linear_regression/lr_usecases.py
@@ -3,21 +3,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 def main():
 
     lineal = LinearRegression()
-    df=pd.read_csv('data/1.csv', sep=',',header=None)
+    df=pd.read_csv('data/8.csv', sep=',',header=None)
     x = df.values[:, 0]
     y = df.values[:, 1]
     data = lineal.fit(x,y)
-    print(data._slope)
-    print(data._intercept)
+    lineal.predict(x)
+    r_square = lineal.r_square(y)
+    print(r_square)
 
-    print(lineal.predict(8))
 
-    plt.scatter(x, y)
-    plt.plot(x,lineal.predict(x))
-    plt.show()
+    # plt.scatter(x, y)
+    # plt.plot(x,lineal.predict(x))
+    # plt.show()
 
 
 if __name__ == "__main__":
