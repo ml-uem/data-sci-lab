e243343f4f883aa099d10a9846b6b9269523e6ef
Juan Luis Trejo Medina <trejomedina@yahoo.com>
2020-01-16 23:51:02 +0100

    Add comment (Prueba branch)

---
+++ mluem/MultipleLinearRegression.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class MultipleLinearRegression:
+    """
+    Multiple Linear Regression Alg
+    """
+    def __init__(self):
+        A = 0
+
+    def fit(self, X, y):
+
+       w = np.linalg.solve(np.dot(X,np.transpose(X)),np.dot(np.transpose(X),y))
+
+        return self
+
+  def predict(self, x):
+    """
+    """
+    return w * x 
+
+  def rsquare(self, x, y):
+    n = y - self.predict(x)
+    m = y - np.mean(y)
+    return 1 - ( n.dot(n) / m.dot(m) )
