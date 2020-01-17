4e9725c9e67940e5d4efffc3299cc8075cd26ea7
Victor <victorcpascua@gmail.com>
2020-01-17 19:37:16 +0100

    Added project url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='ml_uem',
       version='0.2',
       description='Machine Learning UEM Package',
+      url='https://github.com/ml-uem/data-sci-lab',
       packages=['mluem'],
       install_requires=['numpy'],
       zip_safe=False)
