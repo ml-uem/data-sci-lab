81e252b0391c4511b298dfef6f6c5d70c5c0ff47
Juan Luis Trejo Medina <trejomedina@yahoo.com>
2020-01-24 17:43:47 +0100

    Adding Distance Euclidea Function

--- mluem/Utils.py
+++ mluem/Utils.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 class Utils:
-    def __init__(self):
-        return
-
-    def Sigmoid(self, z):
+    def Sigmoid(self,z):
         return (1/(1+np.exp(-z)))
 
-    def CrossEntropyLoss(self, Y, A, m):
-        return np.dot(-1 / m, (np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))))
+    def CrossEntropy(self, a,y):
+        return np.mean(y * np.log(a) + (1-y) * np.log(1-a))
+
+    def DistEuclidea (self,dato,dato1):
+        x=0
+        for i in range(len(dato1.columns)-1):
+            x += (((dato.iloc[0,i] - dato1.iloc[:,i])**2))
+        return np.sqrt(x)
