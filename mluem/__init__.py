0372817f24dbef2d9c3e5e18ee73f049b9c5c5c3
Juan Luis Trejo Medina <trejomedina@yahoo.com>
2020-01-24 17:41:28 +0100

    Including in Utils the Distance Euclidea Function

--- mluem/__init__.py
+++ mluem/__init__.py
@@ -2,4 +2,5 @@ from .ModelInterface import ModelInterface
 
 from .SimpleLinearRegression import SimpleLinearRegression
 from .MultipleLinearRegression import MultipleLinearRegression
+from .KNN import KNN
 from .Utils import Utils
