4fc23d463c9b8586267b44d55659033408edbb6a
popigg <popigg@gmail.com>
2020-01-25 14:30:57 +0100

    add test for simple linear regression

---
+++ samples/slr.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mluem.SimpleLinearRegression as slr
+
+def main():
+    lineal = slr()    
+    file = '' # add your path
+    df = pd.read_csv('', sep=',',header=None)
+    x = df.values[:, 0]
+    y = df.values[:, 1]
+
+    data = lineal.fit(x,y)
+    print(test(y, lineal.predict(x)))
+    print('score ==>', lineal.score(x,y))   
+
+    plt.scatter(x, y)
+    plt.plot(x,lineal.predict(x))
+    plt.show()
+
+
+def test(y, prediction):
+    n = y - prediction
+    m = y - np.mean(y)
+
+    return 1 - ( n.dot(n) / m.dot(m) )
+
+
+if __name__ == "__main__":
+    main()
