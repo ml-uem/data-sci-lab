9b355a0005992627b116e486b800ce3a18801e2a
santiago.quintero <santiago.quintero@outsourcing.cepsa.com>
2020-02-22 11:09:14 +0100

    Hotfix: Error with module names

--- mluem/__init__.py
+++ mluem/__init__.py
@@ -3,7 +3,6 @@ from .ModelInterface import ModelInterface
 from .SimpleLinearRegression import SimpleLinearRegression
 from .MultipleLinearRegression import MultipleLinearRegression
 from .SingleLayerNeuralNetwork import SingleLayerNeuralNetwork
-from .Logisticregression import LogisticRegression
-from .SNLL import SNLL
+from .LogisticRegression import LogisticRegression
 from .Utils import Utils
 from .KNN import KNN #KNN Alg Binary classification and regression
