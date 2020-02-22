f7918cafe0e3cf770c2efc0ff748fbcd2ffd845b
santiago.quintero <santiago.quintero@outsourcing.cepsa.com>
2020-02-22 11:20:25 +0100

    Removed comment

--- mluem/__init__.py
+++ mluem/__init__.py
@@ -5,4 +5,4 @@ from .MultipleLinearRegression import MultipleLinearRegression
 from .SingleLayerNeuralNetwork import SingleLayerNeuralNetwork
 from .LogisticRegression import LogisticRegression
 from .Utils import Utils
-from .KNN import KNN #KNN Alg Binary classification and regression
+from .KNN import KNN
